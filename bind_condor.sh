d83842aba33ca85e66871424db71f565f5406436
Kevin Pedro <kpedro88@gmail.com>
2024-12-20 04:02:42 -0600

    fix behavior when sourced

--- bind_condor.sh
+++ bind_condor.sh
@@ -4,7 +4,7 @@ LPC_CONDOR_CONFIG=/etc/condor/config.d/01_cmslpc_interactive
 LPC_CONDOR_LOCAL=/usr/local/bin/cmslpc-local-conf.py
 
 # not all containers have /usr/bin/python3
-COMMAND_NAME=$(basename "$0")
+COMMAND_NAME=$(basename -- "$0")
 COMMAND_PATH=$(readlink -f "${BASH_SOURCE[0]}")
 if [ "$COMMAND_NAME" = "$(basename $LPC_CONDOR_LOCAL)" ]; then
 	python3 ${LPC_CONDOR_LOCAL}.orig | grep -v "LOCAL_CONFIG_FILE"
